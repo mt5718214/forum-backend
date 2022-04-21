8e97897a43eaac180105b4d66e6f2816cc13bdf9
tsungilin <mt5718214@gmail.com>
2022-04-21 09:59:17 +0800

    移除後端render檔案

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-let routes = require('./routes')
 let apis = require('./apis')
 let chat = require('./chat')
 
 module.exports = (app) => {
   app.use('/chat', chat)
-  app.use('/', routes)
   app.use('/api', apis)
 }
